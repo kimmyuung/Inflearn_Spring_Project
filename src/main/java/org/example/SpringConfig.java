73045e087fa337878fb782bca0a3693d57be2bed
kimmyungho <kimmyungho1003@naver.com>
2023-08-18 06:08:16 +0900

    회원 가입과 회원 목록 구현

--- src/main/java/org/example/SpringConfig.java
+++ src/main/java/org/example/SpringConfig.java
@@ -6,17 +6,17 @@ import repository.MemberRepository;
 import repository.MemoryMemberRepository;
 import service.MemberService;
 
-@Configuration // 컴포넌트 스캔을 하지 않을 수 사용하는 방법
-// 스프링 빈 직접 등록
-public class SpringConfig {
-
-    @Bean
-    public MemberService memberService() {
-        return new MemberService(memberRepository());
-    }
-
-    @Bean
-    public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
-    }
-}
+//@Configuration // 컴포넌트 스캔을 하지 않을 수 사용하는 방법
+//// 스프링 빈 직접 등록
+//public class SpringConfig {
+//
+//    @Bean
+//    public MemberService memberService() {
+//        return new MemberService(memberRepository());
+//    }
+//
+//    @Bean
+//    public MemberRepository memberRepository() {
+//        return new MemoryMemberRepository();
+//    }
+//}
